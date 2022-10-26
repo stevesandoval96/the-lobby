e45e98bc12da98362e7676980f00cc8023ec4921
Steve Sandoval <sandoval_steve96@yahoo.com>
2022-10-26 14:26:46 -0500

    Working logo change

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,18 @@
-const logo = document.getElementById('logo');
-const logoGlitchedTwo = document.getElementById('logo-glitched-two');
-const logoGlitchedThree = document.getElementById('logo-glitched-three');
-
-function checkImage() {
-    if (logo.src === "../Images/Sandoval Stable White.png") {
-        logo.src = "../Images/Sandoval Glitch Two White.png";
-    } else if (logo.src === "../Images/Sandoval Glitch Two White.png") {
-        logo.src = "../Images/Sandoval Glitch Three White.png"
-    } else {
-        logo.src = "../Images/Sandoval Stable White.png"
+function changeImage() {
+    let i = 0;
+    let images = ['../Images/Sandoval Glitch Two White.png', '../Images/Sandoval Glitch Three White.png', '../Images/Sandoval Stable White.png', '../Images/Sandoval Glitch Two White.png', '../Images/Sandoval Glitch Three White.png', '../Images/Sandoval Stable White.png', '../Images/Sandoval Glitch Two White.png', '../Images/Sandoval Glitch Three White.png']
+    const logo = document.getElementById('logo');
+    const imageInterval = setInterval(toggle, 125);
+    function toggle() {
+        logo.src = images[i];
+        i++
+        if (i == images.length) {
+            clearInterval(imageInterval);
+            i = 0;
+            logo.src='../Images/Sandoval Stable White.png';
+        }
     }
 }
-function setNewImages() {
-    setTimeout(checkImage, 1000)
-}
 
-function setLogo() {
 
-}
+
